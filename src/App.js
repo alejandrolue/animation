fa9388b0b12c9d316e9b1303602013c41c125aef
Alejandro <you@example.com>
2022-03-04 14:41:15 +0100

    Set the Layout of the Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header";
+import BasicTabs from "./Components/BasicTabs";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    basicTabs: {
+        position: 'absolute',
+        marginTop: '3%',
+        width: '100%',
+        },
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles()
+    return (
+        <div className="App">
+            <div className="Header">
+                <Header/>
+            </div>
+            <div className={classes.basicTabs}>
+                <BasicTabs/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
