571e0c742c0af5d991692f39146f9904ed77d2d3
Alejandro <you@example.com>
2022-03-11 10:46:16 +0100

    Circle works now

--- src/Sites/CircleSite.js
+++ src/Sites/CircleSite.js
@@ -3,10 +3,11 @@ import Logo from "../svg/logo.svg";
 
 
 export default function CircleSite (){
-    const [value, setValue] = useState();
+    const [value, setValue] = useState(10);
+
     return (
         <div className="App">
-            <input value={}/>
+            <input value={value} onChange={e => setValue(e.target.value)}/>
 
             <svg height="500px" width="90%" >
                 <circle r={value} fill="red" cx="50%" cy="50%"/>
